78f671dde9b91ca78216371fed6af498786f950f
dbaric99 <damjana.baric1999@gmail.com>
2019-07-17 12:54:04 +0200

    Making ICar interface and 3 implementations

---
+++ Reflection/ICar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    public interface ICar
+    {
+        string Name { get; set; }
+        string Model { get; set; }
+        string Engine { get; set; }
+        double Wheels { get; set; }
+    }
+}
---
+++ Reflection/Mercedes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    public class Mercedes :ICar
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        private string _model;
+        public string Model
+        {
+            get => _model;
+            set => _model = value;
+        }
+
+        private string _engine;
+        public string Engine
+        {
+            get => _engine;
+            set => _engine = value;
+        }
+
+        private double _wheels;
+        public double Wheels
+        {
+            get => _wheels;
+            set => _wheels = value;
+        }
+    }
+}
