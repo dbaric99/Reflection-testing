bd113e72da62679a5794b07fbd9602ab105dc90c
dbaric99 <damjana.baric1999@gmail.com>
2019-08-01 13:33:44 +0200

    Closed #4

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -6,12 +6,13 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Reflection
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             const string resourceName = "ReflectionTextFile.txt";
             var location = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
@@ -21,15 +22,13 @@ namespace Reflection
             {
                 var type = new List<string>();
                 var assembly = Assembly.GetExecutingAssembly();
-                string resource = assembly.GetManifestResourceNames().Single(str => str.EndsWith("ReflectionTextFile.txt"));
-                using (Stream stream = assembly.GetManifestResourceStream(resource))
+                var resource = assembly.GetManifestResourceNames().Single(str => str.EndsWith("ReflectionTextFile.txt"));
+                using (Stream stream =  assembly.GetManifestResourceStream(resource))
                 using (var reader = new StreamReader(stream))
                 {
                     string line;
                     while ((line = reader.ReadLine()) != null)
-                    {
                         type.Add(line);
-                    }
                 }
 
                 var firstCar = assembly.CreateInstance(type[0]) as ICar;
@@ -42,7 +41,8 @@ namespace Reflection
             else
                 Console.WriteLine("The targeted text file does not exist!");
 
-            Console.ReadKey();
+            ConsoleKey key = default;
+            await Task.Run(() => key = Console.ReadKey(true).Key);
         }
     }
 }
